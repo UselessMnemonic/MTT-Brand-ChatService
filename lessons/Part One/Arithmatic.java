70110b9701a16cbb79ccb380ae573783a1caca5b
Christopher <Christopher@ProjectCarthage>
2016-04-13 20:56:28 -0700

    Created Part One

---
+++ lessons/Part One/Arithmatic.java
@@ -0,0 +1,18 @@
+
+/*
+ * Create the double variables a and b, then store their quotient to c.
+ * Print c to the console.
+ */
+
+public class Arithmatic {
+	
+	public static void main(String[] args)
+	{
+	_____;
+	_____;
+	_____;
+	
+	_______________;
+	
+	}
+}
---
+++ lessons/Part One/PrintToScreen.java
@@ -0,0 +1,13 @@
+
+public class PrintToScreen {
+
+	/*
+	 * Print to the console the line "Hello World!"
+	 */
+	
+	public static void main(String[] args)
+	{
+		System.out._______;
+	}
+	
+}
