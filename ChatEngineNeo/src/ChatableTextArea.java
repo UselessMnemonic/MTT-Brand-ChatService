0eca63436f4d25923a7db2821b209b4dd32b7042
ta <ta@10.50.200.157>
2016-05-26 08:38:17 -0700

    worth

--- ChatEngineNeo/src/ChatableTextArea.java
+++ ChatEngineNeo/src/ChatableTextArea.java
@@ -2,14 +2,22 @@ import java.awt.TextArea;
 
 public class ChatableTextArea extends TextArea implements Chatable{
 
+	private boolean isFirstMessage;
+	
 	@Override
 	public void onMessage(Object message) {
-		setText(getText() + "\n@" + new java.util.Date() + ": " + (String)message);
+		if(isFirstMessage){
+			isFirstMessage = false;
+			setText(getText() + new java.util.Date().getTime() + ": " + (String)message);
+		}
+		else
+			setText(getText() + "\n@" + new java.util.Date().getTime() + ": " + (String)message);
 	}
 
 	public ChatableTextArea()
 	{
 		super();
+		isFirstMessage = true;
 		setEditable(false);
 	}
 	
--- ChatEngineNeo/src/ClientWindow.java
+++ ChatEngineNeo/src/ClientWindow.java
@@ -4,15 +4,24 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ClientWindow {
 
 	private JFrame frame;
-	private JTextArea textWindow;
-	private JTextField textField;
-	private final JButton btnNewButton = new JButton("Connect");
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private ChatableTextArea textWindow;
+	private JTextField input;
+	private final JButton actionBttn = new JButton("Connect");
+	private JTextField port;
+	private JTextField ip;
+	private JTextField username;
+	private Client client;
 
 	/**
 	 * Launch the application.
@@ -42,30 +51,71 @@ public class ClientWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 616, 300);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				if(client != null)
+					try {
+						client.shutdown();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+			}
+		});
+		frame.setBounds(100, 100, 600, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		textWindow = new JTextArea();
+		textWindow = new ChatableTextArea();
 		textWindow.setBounds(6, 6, 438, 228);
 		frame.getContentPane().add(textWindow);
 		
-		textField = new JTextField();
-		textField.setBounds(6, 246, 308, 26);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		btnNewButton.setBounds(315, 246, 129, 26);
-		frame.getContentPane().add(btnNewButton);
+		input = new JTextField();
+		input.setBounds(6, 246, 308, 26);
+		frame.getContentPane().add(input);
+		input.setColumns(10);
+		actionBttn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(actionBttn.getText().equals("Connect"))
+				{
+					actionBttn.setText("Send");
+					try {
+						client = new Client(ip.getText(), port.getText(), textWindow);
+					} catch (NumberFormatException e1) {
+						textWindow.onMessage("@Error: Check port setting!");
+						actionBttn.setText("Connect");
+					} catch (UnknownHostException e1) {
+						textWindow.onMessage("@Error: Check that the IP is correct!");
+						actionBttn.setText("Connect");
+					} catch (IOException e1) {
+						textWindow.onMessage("@Error: Couldn't connect!");
+						actionBttn.setText("Connect");
+					}
+				}
+			}
+		});
+		actionBttn.setBounds(315, 246, 129, 26);
+		frame.getContentPane().add(actionBttn);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(456, 6, 130, 26);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		port = new JTextField();
+		port.setText("9799");
+		port.setToolTipText("port");
+		port.setBounds(456, 6, 130, 26);
+		frame.getContentPane().add(port);
+		port.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(456, 35, 130, 26);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		ip = new JTextField();
+		ip.setText("ip");
+		ip.setHorizontalAlignment(SwingConstants.CENTER);
+		ip.setToolTipText("ip");
+		ip.setBounds(456, 35, 130, 26);
+		frame.getContentPane().add(ip);
+		ip.setColumns(10);
+		
+		username = new JTextField();
+		username.setText("username");
+		username.setBounds(456, 64, 130, 26);
+		frame.getContentPane().add(username);
+		username.setColumns(10);
 	}
-
 }
